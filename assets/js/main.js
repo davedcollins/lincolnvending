4c118209715e521e3a18a62e6b94da7a755cf2ad
davedcollins <58882568+davedcollins@users.noreply.github.com>
2022-02-14 15:25:59 +0000

    added contact form, updated images

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,8 +46,9 @@ $(function() {
 
         scrollLink.each(function() {
 
+        if ($(this.hash).offset()){
           var sectionOffset = $(this.hash).offset().top - 73;
-
+      }
           if ( sectionOffset <= scrollbarLocation ) {
             $(this).parent().addClass('active');
             $(this).parent().siblings().removeClass('active');
